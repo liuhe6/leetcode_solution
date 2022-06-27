90af77f454a08f3d9015282bbfbd0ecdbd162573
liuhe6 <liuhe524@163.com>
2022-06-27 21:50:16 +0800

    Create 101_对称二叉树.cpp

---
+++ 101_对称二叉树.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root) return false;
+        return dfs(root->left, root->right);
+    }
+    bool dfs(TreeNode* p, TreeNode* q) {
+        if(!p && !q) return true;
+        if(!p || !q || p->val != q->val) return false;
+        return dfs(p->left, q->right) && dfs(p->right, q->left);
+    }
+};
