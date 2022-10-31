11d6814f4d4154527763b261b12650e72be2414a
liuhe6 <liuhe524@163.com>
2022-10-31 17:12:08 +0800

    Update 6_ Z 字形变换.cpp
    添加注释

--- 6_ Z 字形变换.cpp
+++ 6_ Z 字形变换.cpp
@@ -6,13 +6,13 @@ public:
         }
 
         vector<string> lines(numRows);
-        int i = 0, flag = 1;
+        int i = 0, flag = 1; //flag控制的是方向，1是向下，-1 是向上
         for (char c : s) {
             lines[i].push_back(c);
             i += flag;
             if (i == -1 || i == numRows) {
-                flag = -flag;
-                i += 2 * flag;
+                flag = -flag; // 当到底或者到顶的时候，改变方向
+                i += 2 * flag; // 改变方向后，并回到正确的位置，以i = -1为例，这时i=0的位置已经添加完成，需要添加的是i=1的位置，所以是i+=2
             }
         }
 
