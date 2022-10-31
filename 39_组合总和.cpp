aaa46052074c91c6c394411be1fcf53c98a478a4
liuhe6 <liuhe524@163.com>
2022-10-31 15:23:25 +0800

    Update 39_组合总和.cpp
    更新39题答案

--- 39_组合总和.cpp
+++ 39_组合总和.cpp
@@ -7,16 +7,14 @@ public:
         return res;
     }
     void dfs(vector<int>& candidates, int u, int target) {
-        if(target == 0) {
+        if (target == 0) {
             res.push_back(path);
             return;
-        }
-        if(u == candidates.size()) return;
-        for(int i = 0; candidates[u] * i <= target; i++ ){
-            dfs(candidates, u + 1, target - candidates[u] * i);
-            path.push_back(candidates[u]);
-        }
-        for(int i = 0; candidates[u] * i <= target; i++ ){
+        } 
+        for (int i = u; i < candidates.size(); i++) {
+            if(candidates[i] > target) continue;
+            path.push_back(candidates[i]);
+            dfs(candidates, i , target - candidates[i]);
             path.pop_back();
         }
     }
